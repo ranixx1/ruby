564d9aa40ea66a7e98dacafadb83213328d9639c
ranilton <ranilton.l@escolar.ifrn.edu.br>
2025-04-19 02:18:42 -0300

    estruturas de repetição

---
+++ lib/condicionais/if.rb
@@ -0,0 +1,25 @@
+# # x = 10
+
+# # if x == 10
+# #     puts "x é igual a 10"
+
+# # elsif == 50
+# #     puts"x é igual a 50"
+
+# # else 
+# #     puts "x não é igual a 10"
+# # end
+
+
+# nome = "diego"
+
+# if nome == "diego"
+#     puts "que nome feio"
+
+# elsif nome =="joão"
+#     puts "que nome lindo"
+
+# else
+#     puts " que nome maravilhoso"
+
+# end
