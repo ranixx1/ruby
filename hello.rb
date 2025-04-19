8670c19b4cc3d908fbea330568aa45fd2b00f4cb
ranilton <ranilton.l@escolar.ifrn.edu.br>
2025-04-19 02:11:14 -0300

    hello world

---
+++ hello.rb
@@ -0,0 +1,8 @@
+puts "hello world"
+
+#pedindo entrada de informação
+nome= gets.chomp
+idade = gets.chomp.to_i #to_i trasnforma o numéro em inteiro
+#imprimindo
+puts "bem vindo, #{nome}"
+puts "eu tenho #{idade}"
