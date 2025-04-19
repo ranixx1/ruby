71b4293fe3fc9dd078d4aab7e5886f22275e57d2
ranilton <ranilton.l@escolar.ifrn.edu.br>
2025-04-19 02:18:57 -0300

    booleanos

---
+++ lib/condicionais/booleanos.rb
@@ -0,0 +1,24 @@
+# AND
+# Verdade e Verdade = Verdade
+# Verdade e Falso   = Falso
+# Falso e Falso     = Falso
+# Falso e Verdade   = Falso
+
+# OR 
+# Verdade ou Verdade = Verdade
+# Verdade ou Falso   = Verdade
+# Falso ou Falso     = Falso
+# Falso ou Verdade   = Verdade
+
+# NOT
+# Não Verdade = Falso
+# Não Falso   = Verdade
+
+puts false && true   # false
+puts false || true   # true
+puts !false && true  # true
+
+# Operadores lógicos:
+# &&  -> AND
+# ||  -> OR
+# !   -> NOT
