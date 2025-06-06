8cab78de9dff7b1f2c6bb98bd6717de87ff31f43
ranilton <ranilton.c@escolar.ifrn.edu.br>
2025-06-06 03:09:19 -0300

    feat: verifica in 2 array

---
+++ arrays/verifica.rb
@@ -0,0 +1,6 @@
+operating_systems = ["Fedora", "SuSE", "RHEL", "Windows", "MacOS"]
+linux_systems = ["RHEL", "SuSE", "PCLinuxOS", "Ubuntu", "Fedora"]
+
+common_systems = operating_systems & linux_systems
+
+p common_systems
